3a38ef0349056d83579ca789588e762ba52df053
Admin <dnarod.2004@gmail.com>
2024-04-23 19:15:43 +0300

    auth update

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,11 +3,9 @@ const signin = document.querySelector(".signin");
 const welcome = document.getElementById("welcome");
 let spanId = document.getElementById("user_id");
 
-if (localStorage.getItem('userId')) {
-  signin.classList.remove("signin_active");
-  welcome.classList.add("welcome_active");
-  spanId.textContent = localStorage.getItem('userId');
-}
+signin.classList.remove("signin_active");
+welcome.classList.add("welcome_active");
+//spanId.textContent = localStorage.getItem('userId');
 
 signinForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -15,20 +13,20 @@ signinForm.addEventListener('submit', (e) => {
   const formData = new FormData(signinForm);
   const xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+  xhr.responseType = 'json';
   xhr.send(formData);
 
   xhr.addEventListener('load', () => {
-    if (xhr.readyState === xhr.DONE || xhr.status === 200) {
-      let response = JSON.parse(xhr.response);
-      if (response.success) {
-        signin.classList.remove("signin_active");
-        welcome.classList.add("welcome_active");
-        spanId.textContent = response.user_id;
-        localStorage.setItem('userId', response.user_id);
-      } else {
-        alert("Неверный логин/пароль");
-      }
+    let response = xhr.response;
+    console.log(response);
+    if (response.success) {
+      signin.classList.remove("signin_active");
+      welcome.classList.add("welcome_active");
+      localStorage.setItem('userId', response.user_id);
+      spanId.textContent = localStorage.getItem('userId');
+    } else {
+      alert("Неверный логин/пароль");
+      signinForm.reset();
     }
   })
 })
-
